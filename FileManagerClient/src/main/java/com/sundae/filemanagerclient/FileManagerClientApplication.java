435498a873b0e2441abca02392f417043992914c
sundae <948820549@qq.com>
2019-11-18 13:55:22 +0800

    add client spring project

---
+++ FileManagerClient/src/main/java/com/sundae/filemanagerclient/FileManagerClientApplication.java
@@ -0,0 +1,13 @@
+package com.sundae.filemanagerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagerClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileManagerClientApplication.class, args);
+    }
+
+}
---
+++ FileManagerClient/src/test/java/com/sundae/filemanagerclient/FilemanagerclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sundae.filemanagerclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilemanagerclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
